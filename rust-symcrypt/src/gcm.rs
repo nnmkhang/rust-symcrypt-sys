9f36923fcb16a06b7e77ff810bf39d11a97ee07c
nnmkhang <khangnguyen@microsoft.com>
2023-07-31 14:38:23 -0700

    panic on encrypt_part mismatch lenght, added test as well

--- rust-symcrypt/src/gcm.rs
+++ rust-symcrypt/src/gcm.rs
@@ -22,7 +22,7 @@ struct GcmStateInner {
 /// Using an inner GCM state that is Pin<Box<T>> since the memory address for Self is moved around when returning from GcmState::new()
 /// My guess is that it has to do with Result<> wrapping the Self and SymCryptError.
 ///
-/// auth_part, encrypt_part, decrypt_part take in an allocated buffer as an out parameter for performance reasons. This is for scenarios
+/// encrypt_part, decrypt_part take in an allocated buffer as an out parameter for performance reasons. This is for scenarios
 /// such as encrypting over a stream of data; allocating and copying data from a return will be costly performance wise.
 ///
 /// Since auth_part, encrypt_part and decrypt_part may be called multiple times, you must call encrypt_final/decrypt_final at the end to ensure
@@ -74,6 +74,7 @@ impl GcmState {
     // Can be called multiple times
     // Takes in a buffer as an out parameter for performance reasons
     pub fn encrypt_part(&mut self, plain_text: &[u8], cipher_text_buffer: &mut [u8]) {
+        assert_eq!(plain_text.len(), cipher_text_buffer.len(), "plain_text and cipher_text_buffer must be the same length");
         unsafe {
             // SAFETY: FFI calls
             symcrypt_sys::SymCryptGcmEncryptPart(
@@ -99,6 +100,7 @@ impl GcmState {
     // Can be called multiple times
     // Takes in a buffer as an out parameter for performance reasons
     pub fn decrypt_part(&mut self, cipher_text: &[u8], plain_text_buffer: &mut [u8]) {
+        assert_eq!(cipher_text.len(), plain_text_buffer.len(), "cipher_text and plain_text_buffer must be the same length");
         unsafe {
             // SAFETY: FFI calls
             symcrypt_sys::SymCryptGcmDecryptPart(
@@ -331,6 +333,26 @@ mod test {
     }
 
     #[test]
+    #[should_panic]
+    fn test_gcm_encrypt_panics_on_length_mismatch() {
+        let p_key = hex::decode("feffe9928665731c6d6a8f9467308308").unwrap();
+        let nonce = hex::decode("cafebabefacedbaddecaf888").unwrap();
+        let auth_data = hex::decode("feedfacedeadbeeffeedfacedeadbeefabaddad2").unwrap();
+        let expected_result = "42831ec2217774244b7221b784d0d49ce3aa212f2c02a4e035c17e2329aca12e21d514b25466931c7d8f6a5aac84aa051ba30b396a0aac973d58e091";
+        let pt = hex::decode("d9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39").unwrap();
+        let mut ct = vec![0; 6];
+
+        let cipher = BlockCipherType::AesBlock;
+
+        let nonce_array: [u8; 12] = nonce.as_slice().try_into().unwrap();
+
+        let mut gcm_state = GcmState::new(&p_key, &nonce_array, cipher).unwrap();
+        gcm_state.auth_part(&auth_data);
+        gcm_state.encrypt_part(&pt, &mut ct);
+        assert_eq!(hex::encode(ct), expected_result);
+    }
+
+    #[test]
     fn test_gcm_encrypt() {
         let p_key = hex::decode("feffe9928665731c6d6a8f9467308308").unwrap();
         let nonce = hex::decode("cafebabefacedbaddecaf888").unwrap();
@@ -373,7 +395,7 @@ mod test {
         assert_eq!(hex::encode(pt), expected_result);
         gcm_state.decrypt_final(&tag).unwrap();
     }
-    
+
     #[test]
     fn test_gcm_decrypt_no_decrypt_part() {
         let p_key = hex::decode("00000000000000000000000000000000").unwrap();
