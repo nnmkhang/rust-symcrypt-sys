da83beb1584936984d5fac21564e2602e2f2cb84
nnmkhang <khangnguyen@microsoft.com>
2023-08-30 13:58:59 -0700

    adding hashing trait pinning

--- rust-symcrypt/src/hmac.rs
+++ rust-symcrypt/src/hmac.rs
@@ -1,11 +1,13 @@
 use core::ffi::c_void;
 use std::mem;
 use std::ptr;
+use std::pin::Pin;
 use symcrypt_sys;
 
 pub const SHA256_HMAC_RESULT_SIZE: usize = symcrypt_sys::SYMCRYPT_SHA256_RESULT_SIZE as usize;
 pub const SHA384_HMAC_RESULT_SIZE: usize = symcrypt_sys::SYMCRYPT_SHA384_RESULT_SIZE as usize;
 
+/// Generic trait for HMACs
 pub trait Hmac {
     type Result;
 
@@ -13,24 +15,34 @@ pub trait Hmac {
     fn result(&mut self) -> Self::Result;
 }
 
-pub struct HmacSha256State {
+pub struct HmacSha256State{
+    inner: Pin<Box<HmacSha256Inner>>,
+}
+
+struct HmacSha256Inner {
     state: symcrypt_sys::SYMCRYPT_HMAC_SHA256_STATE,
     expanded_key: symcrypt_sys::SYMCRYPT_HMAC_SHA256_EXPANDED_KEY,
 }
 
+/// Using an inner HmacSha256 state that is Pin<Box<T>> since the memory address for Self is moved around when returning from HmacSha256State::new()
+///
+/// SymCrypt expects the address for its structs to stay static through the structs lifetime to guarantee that structs are not memcpy'd as
+/// doing so would lead to use-after-free and inconsistent states.
 impl HmacSha256State {
     pub fn new(key: &[u8]) -> Self {
         let mut instance = HmacSha256State {
-            state: symcrypt_sys::SYMCRYPT_HMAC_SHA256_STATE::default(),
-            expanded_key: symcrypt_sys::SYMCRYPT_HMAC_SHA256_EXPANDED_KEY::default(),
+            inner: Box::pin(HmacSha256Inner { 
+                state: symcrypt_sys::SYMCRYPT_HMAC_SHA256_STATE::default(),
+                expanded_key: symcrypt_sys::SYMCRYPT_HMAC_SHA256_EXPANDED_KEY::default() 
+            }),
         };
-        unsafe {
+        unsafe { // SAFETY: FFI calls
             symcrypt_sys::SymCryptHmacSha256ExpandKey(
-                &mut instance.expanded_key,
+                &mut instance.inner.expanded_key,
                 key.as_ptr(),
                 key.len() as symcrypt_sys::SIZE_T,
             );
-            symcrypt_sys::SymCryptHmacSha256Init(&mut instance.state, &instance.expanded_key);
+            symcrypt_sys::SymCryptHmacSha256Init(&mut instance.inner.state, &instance.inner.expanded_key);
         }
         instance
     }
@@ -40,9 +52,9 @@ impl Hmac for HmacSha256State {
     type Result = [u8; SHA256_HMAC_RESULT_SIZE];
 
     fn append(&mut self, data: &[u8]) {
-        unsafe {
+        unsafe { // SAFETY: FFI calls
             symcrypt_sys::SymCryptHmacSha256Append(
-                &mut self.state,
+                &mut self.inner.state,
                 data.as_ptr(),
                 data.len() as symcrypt_sys::SIZE_T,
             )
@@ -51,30 +63,8 @@ impl Hmac for HmacSha256State {
 
     fn result(&mut self) -> Self::Result {
         let mut result = [0u8; SHA256_HMAC_RESULT_SIZE];
-        unsafe {
-            symcrypt_sys::SymCryptHmacSha256Result(&mut self.state, result.as_mut_ptr());
-        }
-        result
-    }
-}
-
-
-impl HmacSha256State {
-
-    fn append(&mut self, data: &[u8]) {
-        unsafe {
-            symcrypt_sys::SymCryptHmacSha256Append(
-                &mut self.state,
-                data.as_ptr(),
-                data.len() as symcrypt_sys::SIZE_T,
-            )
-        }
-    }
-
-    fn result(&mut self) -> [u8; SHA256_HMAC_RESULT_SIZE] {
-        let mut result = [0u8; SHA256_HMAC_RESULT_SIZE];
-        unsafe {
-            symcrypt_sys::SymCryptHmacSha256Result(&mut self.state, result.as_mut_ptr());
+        unsafe { // SAFETY: FFI calls
+            symcrypt_sys::SymCryptHmacSha256Result(&mut self.inner.state, result.as_mut_ptr());
         }
         result
     }
@@ -82,22 +72,22 @@ impl HmacSha256State {
 
 impl Drop for HmacSha256State {
     fn drop(&mut self) {
-        unsafe {
+        unsafe { // SAFETY: FFI calls
             symcrypt_sys::SymCryptWipe(
-                ptr::addr_of_mut!(self.state) as *mut c_void,
-                mem::size_of_val(&mut self.state) as symcrypt_sys::SIZE_T,
+                ptr::addr_of_mut!(self.inner.state) as *mut c_void,
+                mem::size_of_val(&mut self.inner.state) as symcrypt_sys::SIZE_T,
             );
 
             symcrypt_sys::SymCryptWipe(
-                ptr::addr_of_mut!(self.expanded_key) as *mut c_void,
-                mem::size_of_val(&mut self.expanded_key) as symcrypt_sys::SIZE_T,
+                ptr::addr_of_mut!(self.inner.expanded_key) as *mut c_void,
+                mem::size_of_val(&mut self.inner.expanded_key) as symcrypt_sys::SIZE_T,
             );
         }
     }
 }
 
 pub fn hmac_sha256(key: &[u8], data: &[u8], result: &mut [u8; SHA256_HMAC_RESULT_SIZE]) {
-    unsafe {
+    unsafe { // SAFETY: FFI calls
         let mut expanded_key = symcrypt_sys::SYMCRYPT_HMAC_SHA256_EXPANDED_KEY::default();
         symcrypt_sys::SymCryptHmacSha256ExpandKey(
             &mut expanded_key,
@@ -117,24 +107,41 @@ pub fn hmac_sha256(key: &[u8], data: &[u8], result: &mut [u8; SHA256_HMAC_RESULT
     }
 }
 
+
 pub struct HmacSha384State {
+    inner: Pin<Box<HmacSha384Inner>>,
+}
+
+struct HmacSha384Inner {
     state: symcrypt_sys::SYMCRYPT_HMAC_SHA384_STATE,
     expanded_key: symcrypt_sys::SYMCRYPT_HMAC_SHA384_EXPANDED_KEY,
 }
 
+// /// doing so would lead to use-after-free and inconsistent states.
+// impl HmacSha256State {
+//     pub fn new(key: &[u8]) -> Self {
+//         let mut instance = HmacSha256State {
+//             inner: Box::pin(HmacSha256Inner { 
+//                 state: symcrypt_sys::SYMCRYPT_HMAC_SHA256_STATE::default(),
+//                 expanded_key: symcrypt_sys::SYMCRYPT_HMAC_SHA256_EXPANDED_KEY::default() 
+//             }),
+//         };
+
 impl HmacSha384State {
     pub fn new(key: &[u8]) -> Self {
         let mut instance = HmacSha384State {
-            state: symcrypt_sys::SYMCRYPT_HMAC_SHA384_STATE::default(),
-            expanded_key: symcrypt_sys::SYMCRYPT_HMAC_SHA384_EXPANDED_KEY::default(),
+            inner: Box::pin(HmacSha384Inner {
+                state: symcrypt_sys::SYMCRYPT_HMAC_SHA384_STATE::default(),
+                expanded_key: symcrypt_sys::SYMCRYPT_HMAC_SHA384_EXPANDED_KEY::default(),
+            }),
         };
-        unsafe {
+        unsafe { // SAFETY: FFI calls
             symcrypt_sys::SymCryptHmacSha384ExpandKey(
-                &mut instance.expanded_key,
+                &mut instance.inner.expanded_key,
                 key.as_ptr(),
                 key.len() as symcrypt_sys::SIZE_T,
             );
-            symcrypt_sys::SymCryptHmacSha384Init(&mut instance.state, &instance.expanded_key);
+            symcrypt_sys::SymCryptHmacSha384Init(&mut instance.inner.state, &instance.inner.expanded_key);
         }
         instance
     }
@@ -144,9 +151,9 @@ impl Hmac for HmacSha384State {
     type Result = [u8; SHA384_HMAC_RESULT_SIZE];
 
     fn append(&mut self, data: &[u8]) {
-        unsafe {
+        unsafe { // SAFETY: FFI calls
             symcrypt_sys::SymCryptHmacSha384Append(
-                &mut self.state,
+                &mut self.inner.state,
                 data.as_ptr(),
                 data.len() as symcrypt_sys::SIZE_T,
             )
@@ -155,8 +162,8 @@ impl Hmac for HmacSha384State {
 
     fn result(&mut self) -> Self::Result {
         let mut result = [0u8; SHA384_HMAC_RESULT_SIZE];
-        unsafe {
-            symcrypt_sys::SymCryptHmacSha384Result(&mut self.state, result.as_mut_ptr());
+        unsafe { // SAFETY: FFI calls
+            symcrypt_sys::SymCryptHmacSha384Result(&mut self.inner.state, result.as_mut_ptr());
         }
         result
     }
@@ -164,22 +171,22 @@ impl Hmac for HmacSha384State {
 
 impl Drop for HmacSha384State {
     fn drop(&mut self) {
-        unsafe {
+        unsafe { // SAFETY: FFI calls
             symcrypt_sys::SymCryptWipe(
-                ptr::addr_of_mut!(self.state) as *mut c_void,
-                mem::size_of_val(&mut self.state) as symcrypt_sys::SIZE_T,
+                ptr::addr_of_mut!(self.inner.state) as *mut c_void,
+                mem::size_of_val(&mut self.inner.state) as symcrypt_sys::SIZE_T,
             );
 
             symcrypt_sys::SymCryptWipe(
-                ptr::addr_of_mut!(self.expanded_key) as *mut c_void,
-                mem::size_of_val(&mut self.expanded_key) as symcrypt_sys::SIZE_T,
+                ptr::addr_of_mut!(self.inner.expanded_key) as *mut c_void,
+                mem::size_of_val(&mut self.inner.expanded_key) as symcrypt_sys::SIZE_T,
             );
         }
     }
 }
 
 pub fn hmac_sha384(key: &[u8], data: &[u8], result: &mut [u8; SHA384_HMAC_RESULT_SIZE]) {
-    unsafe {
+    unsafe { // SAFETY: FFI calls
         let mut expanded_key = symcrypt_sys::SYMCRYPT_HMAC_SHA384_EXPANDED_KEY::default();
         symcrypt_sys::SymCryptHmacSha384ExpandKey(
             &mut expanded_key,
@@ -219,7 +226,8 @@ mod test {
         let p_key = hex::decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
         let data = hex::decode("").unwrap();
         let expected = "915cb2c078aaf5dfb3560cf6d96997e987b2de5cd46f9a2ef92493bfc34bab16";
-        
+        let mut result: [u8; SHA256_HMAC_RESULT_SIZE] = [0; SHA256_HMAC_RESULT_SIZE];
+    
         let mut hmac_test = HmacSha256State::new(&p_key);
         test_generic_hmac_state(hmac_test, &data, expected)        
     }
@@ -230,6 +238,7 @@ mod test {
         let data = hex::decode("").unwrap();
         let expected = "915cb2c078aaf5dfb3560cf6d96997e987b2de5cd46f9a2ef92493bfc34bab16";
         let mut hmac_test = HmacSha256State::new(&p_key);
+        let mut result: [u8; SHA256_HMAC_RESULT_SIZE] = [0; SHA256_HMAC_RESULT_SIZE];
 
 
         hmac_test.append(&data);
@@ -242,6 +251,7 @@ mod test {
         let p_key = hex::decode("0a71d5cf99849bc13d73832dcd864244").unwrap();
         let data = hex::decode("17f1ee0c6767a1f3f04bb3c1b7a4e0d4f0e59e5963c1a3bf1540a76b25136baef425faf488722e3e331c77d26fbbd8300df532498f50c5ecd243f481f09348f964ddb8056f6e2886bb5b2f453fcf1de5629f3d166324570bf849792d35e3f711b041b1a7e30494b5d1316484ed85b8da37094627a8e66003d079bfd8beaa80dc").unwrap();
         let expected = "2a0f542090b51b84465cd93e5ddeeaa14ca51162f48047835d2df845fb488af4";
+        let mut result: [u8; SHA256_HMAC_RESULT_SIZE] = [0; SHA256_HMAC_RESULT_SIZE];
 
         hmac_sha256(&p_key, &data, &mut result);
         assert_eq!(hex::encode(result), expected);
@@ -253,6 +263,7 @@ mod test {
             .unwrap();
         let data = hex::decode("beec952d19e8b3db3a4b7fdb4c1d2ea1c492741ea23ceb92f380b9a29b476eaa51f52b54eb9f096adc79b8e8fb8d675686b3e45466bd0577b4f246537dbeb3d9c2a709e4c383180e7ee86bc872e52baaa8ef4107f41ebbc5799a716b6b50e87c19e976042afca7702682e0a2398b42453430d15ed5c9d62448608212ed65d33a").unwrap();
         let expected = "864c0a933ee2fe540e4444399add1cd94ff6e4e14248eaf6df7127cd12c7a9e0f7bd92b303715c06d1c6481114d22167";
+        let mut result: [u8; SHA384_HMAC_RESULT_SIZE] = [0; SHA384_HMAC_RESULT_SIZE];
 
         let hmac_test = HmacSha384State::new(&p_key);
         test_generic_hmac_state(hmac_test, &data, expected);
@@ -263,6 +274,7 @@ mod test {
         let p_key = hex::decode("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b").unwrap();
         let data = hex::decode("").unwrap();
         let expected = "ad88735f29e167dabded11b57e168f0b773b2985f4c2d2234c8d7a6bf01e2a791590bc0165003f9a7e47c4c687622fd6";
+        let mut result: [u8; SHA384_HMAC_RESULT_SIZE] = [0; SHA384_HMAC_RESULT_SIZE];
 
         hmac_sha384(&p_key, &data, &mut result);
         assert_eq!(hex::encode(result), expected);
